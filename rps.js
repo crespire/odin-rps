8e38328b5527ff7787d65558891299262311435a
crespire <li.simmon@gmail.com>
2021-09-29 12:28:08 -0400

    Finished game logic

--- rps.js
+++ rps.js
@@ -5,20 +5,99 @@ function getRdmInt(max) {
 function computerPlay() {
     let move = undefined;
     let selection = getRdmInt(3);
-    console.log(selection);
 
     switch (selection) {
         case 0:
-            move = "Rock";
+            move = "rock";
             break;
         case 1:
-            move = "Paper";
+            move = "paper";
             break;
         case 2:
-            move = "Scissors";
+            move = "scissors";
             break;
         default:
             move = null;
     }
     return move;
+}
+
+function comparePlay(p, c) {
+    let winner = undefined;
+
+    if (p === c) {
+        winner = "t"
+    } else {
+        if (p === "rock") {
+            if (c === "scissors") {
+                winner = "p"
+            } else {
+                winner = "c"
+            }
+        } else if (p === "paper") {
+            if (c === "rock") {
+                winner = "p";
+            } else {
+                winner = "c";
+            }
+        } else {
+            if (c === "paper") {
+                winner = "c";
+            } else {
+                winner = "p";
+            }
+        }
+    }
+
+    return winner;
+}
+
+function playRound(playerInput, computerInput) {
+    const ALLOWED = ["rock", "paper", "scissors"];
+    let playerSelection = playerInput.toLowerCase();
+    let winner = undefined;
+    let roundResult = undefined;
+
+    if (!ALLOWED.includes(playerSelection)) {
+        return "Invalid move.";
+    }
+
+    console.log(`The inputs for playRound are: \n Player: ${playerSelection} \n Computer: ${computerInput}`);
+    winner = comparePlay(playerSelection, computerInput);
+    console.log(`The winner returned was ${winner}.`);
+
+    if (winner === 'p') {
+        roundResult = `You win! ${playerSelection} beats ${computerInput}!`;
+    } else if (winner === 'c') {
+        roundResult = `You lose! ${playerSelection} beats ${computerInput}!`;
+    } else {
+        roundResult = `It was a tie!`
+    }
+
+    return roundResult;
+}
+
+function game(iteration) {
+    let games = 0;
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+    let message = "";
+
+    for (i=1;i<iteration+1;i++) {
+        message = playRound(computerPlay(), computerPlay());
+        games += 1;
+
+        if (message.includes("win")) {
+            wins += 1;
+        } else if (message.includes("lose")) {
+            losses += 1;
+        } else {
+            ties += 1;
+        }
+
+        console.log(message);
+    }
+
+    console.log(`We just played ${games} games. You won ${wins} times, lost ${losses} times and tied ${ties} times for a win rate of ${Math.round(wins/games * 100)}%!`)
 }
