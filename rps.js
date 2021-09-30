b6682a3f8cafced40efe5d70d96aac67bb760c36
crespire <li.simmon@gmail.com>
2021-09-30 16:56:37 -0400

    updated game to use mouse clicks

--- rps.js
+++ rps.js
@@ -1,7 +1,15 @@
+// Globals to keep track of game statistics
+let gamesPlayed = 0;
+let gamesWon = 0;
+let gamesLost = 0;
+let gamesTied = 0;
+
+// Helper function to generate a random integer based on a provided number
 function getRdmInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Function to pick a move for the computer
 function computerPlay() {
     let move = undefined;
     let selection = getRdmInt(3);
@@ -22,6 +30,7 @@ function computerPlay() {
     return move;
 }
 
+// Helper function to determine the winner, based on player's choice and computer's choice
 function comparePlay(pChoice, cChoice) {
     let winner = undefined;
 
@@ -52,6 +61,7 @@ function comparePlay(pChoice, cChoice) {
     return winner;
 }
 
+// Play round function that returns the winner of a round.
 function playRound(playerInput, computerInput) {
     const ALLOWED = ["rock", "paper", "scissors"];
     let playerSelection = playerInput.toLowerCase();
@@ -64,9 +74,9 @@ function playRound(playerInput, computerInput) {
     }
 
     // Get the winner
-    // console.log(`The inputs for playRound are: \n Player: ${playerSelection} \n Computer: ${computerInput}`);
+    //console.log(`The inputs for playRound are: \n Player: ${playerSelection} \n Computer: ${computerInput}`);
     winner = comparePlay(playerSelection, computerInput);
-    // console.log(`The winner returned was ${winner}.`);
+    //console.log(`The winner returned was ${winner}.`);
 
     // Set the round result message
     if (winner === 'p') {
@@ -80,6 +90,7 @@ function playRound(playerInput, computerInput) {
     return roundResult;
 }
 
+/** Old console based game function
 function game(iteration) {
     let gamesPlayed = 0;
     let gamesWon = 0;
@@ -87,23 +98,58 @@ function game(iteration) {
     let gamesTied = 0;
     let message = "";
 
-    for (i=1;i<iteration+1;i++) {
-        input = window.prompt("What is your play?")
-        message = playRound(input, computerPlay());
-        gamesPlayed += 1;
+    message = playRound(input, computerPlay());
+    gamesPlayed += 1;
 
-        if (message.includes("win")) {
-            gamesWon += 1;
-        } else if (message.includes("lose")) {
-            gamesLost += 1;
-        } else if (message.includes("tie")) {
-            gamesTied += 1;
-        } else {
-            message += " Try again."
-        }
-
-        alert(message);
+    if (message.includes("win")) {
+        gamesWon += 1;
+    } else if (message.includes("lose")) {
+        gamesLost += 1;
+    } else if (message.includes("tie")) {
+        gamesTied += 1;
+    } else {
+        message += " Try again."
     }
 
-    alert(`We played ${gamesPlayed} games. You won ${gamesWon} times, lost ${gamesLost} times and tied ${gamesTied} times for a win rate of ${Math.round(gamesWon/gamesPlayed * 100)}%!`)
+    alert(message);
+}  */
+
+// New game function for GUI version
+function playGame(event) {
+    // Event handling variables
+    let clickedTag = event.target.tagName;
+    let moveSelected = event.target.id;
+
+    // console.log(`Got a click, here's info: \n${event}`);
+    // console.log(`You clicked on a ${clickedTag}`);
+    
+    if (!moveSelected) return; // Exit this round if we don't detect a valid click
+    if (gamesWon >= 5) { // Stop after 5 wins
+        alert('You won!');
+    }; 
+
+    // We have a valid move, so play the round.
+    // console.log(`You played ${moveSelected}`);
+    let message = playRound(moveSelected, computerPlay());
+
+    gamesPlayed += 1;
+
+    if (message.includes("win")) {
+        gamesWon += 1;
+    } else if (message.includes("lose")) {
+        gamesLost += 1;
+    } else if (message.includes("tie")) {
+        gamesTied += 1;
+    } else {
+        message += " Try again."
+    }
+
+    let outputDiv = document.querySelector('.output');
+    let outputP = document.createElement('p');
+    outputP.textContent = message;
+    outputDiv.appendChild(outputP);
+    
 }
+
+// Set up event listener for a button click.
+window.addEventListener('click', playGame);
